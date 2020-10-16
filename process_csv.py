aa3b2e5775605fd2469e317ee0b964878f10ae7a
junhui096 <e0203169@u.nus.edu>
2020-10-16 18:41:22 +0800

    Setup skeleton

---
+++ process_csv.py
@@ -0,0 +1,20 @@
+def process_dataset(input_name, output_name):
+    pass
+
+
+def create_new_row(name, price):
+    pass
+
+
+def get_output_header():
+    pass
+
+def split_name_into_first_and_last_name(name):
+    pass
+
+def remove_leading_zeros(price):
+    pass
+
+def set_above_100(price):
+    pass
+
