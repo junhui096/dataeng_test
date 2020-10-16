d4b79e70fc9c09a85a05854eda376bbf8da8b38d
junhui096 <e0203169@u.nus.edu>
2020-10-16 18:55:34 +0800

    Complete all processing functions

--- process_csv.py
+++ process_csv.py
@@ -1,3 +1,6 @@
+from decimal import Decimal
+
+
 def process_dataset(input_name, output_name):
     with open(input_name, "r") as f:
         header = f.readline()
@@ -17,20 +20,24 @@ def create_new_row(name, price):
     first_name, last_name = split_name_into_first_and_last_name(name)
     price = remove_leading_zeros(price)
     is_above_100 = set_above_100(price)
-    return ",".join([first_name, last_name, price, is_above_100])
+    return ",".join([first_name, last_name, price, str(is_above_100)])
 
 
 def get_output_header():
-    pass
+    return ",".join(["first_name", "last_name", "price", "above_100"])
 
 
 def split_name_into_first_and_last_name(name):
-    pass
+    name_chars = name.split()
+    first_name = name_chars[0:-1]
+    last_name = name_chars[-1]
+    return first_name, last_name
 
 
 def remove_leading_zeros(price):
-    pass
+    return price.lstrip("0")
 
 
 def set_above_100(price):
-    pass
+    return Decimal(price) > 100
+
